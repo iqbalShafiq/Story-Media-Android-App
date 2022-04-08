853d41c7db39a2db832ccf5be8f46bcf88255b8a
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2022-04-09 01:09:35 +0700

    feat: Create register integration api

--- app/src/main/java/space/iqbalsyafiq/storymedia/ui/credential/CredentialViewModel.kt
+++ app/src/main/java/space/iqbalsyafiq/storymedia/ui/credential/CredentialViewModel.kt
@@ -1,7 +1,38 @@
 package space.iqbalsyafiq.storymedia.ui.credential
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import space.iqbalsyafiq.storymedia.model.DataResponse
+import space.iqbalsyafiq.storymedia.model.request.RegisterRequest
+import space.iqbalsyafiq.storymedia.repository.api.ApiConfig
 
 class CredentialViewModel(application: Application): AndroidViewModel(application) {
+    private val service = ApiConfig.getApiService()
+
+    // live data
+    private var _registerUser = MutableLiveData<Boolean>()
+    val registerUser: LiveData<Boolean> = _registerUser
+
+    fun registerUser(requestBody: RegisterRequest) {
+        service.registerUser(requestBody).enqueue(object: Callback<DataResponse> {
+            override fun onResponse(call: Call<DataResponse>, response: Response<DataResponse>) {
+                _registerUser.value = response.isSuccessful
+            }
+
+            override fun onFailure(call: Call<DataResponse>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+                _registerUser.value = false
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = CredentialActivity::class.java.simpleName
+    }
 }
--- app/src/main/java/space/iqbalsyafiq/storymedia/ui/credential/signup/SubmitSignUpFragment.kt
+++ app/src/main/java/space/iqbalsyafiq/storymedia/ui/credential/signup/SubmitSignUpFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import space.iqbalsyafiq.storymedia.databinding.FragmentSubmitSignUpBinding
+import space.iqbalsyafiq.storymedia.ui.credential.CredentialViewModel
 
 class SubmitSignUpFragment : Fragment() {
 
     private val args: SubmitSignUpFragmentArgs by navArgs()
+    private val viewModel: CredentialViewModel by activityViewModels()
     private var _binding: FragmentSubmitSignUpBinding? = null
     private val binding get() = _binding!!
 
@@ -52,6 +55,7 @@ class SubmitSignUpFragment : Fragment() {
                          * check to API for duplicate validation
                          * intent to dashboard after registration success
                          */
+
                         Toast.makeText(
                             requireContext(),
                             "Registration Success!",
